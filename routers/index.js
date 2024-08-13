db6e92b25d7a911a09e68c72af20dacee68f10ee
Adejare <adejare.arasheed@gmail.com>
2024-08-13 20:04:51 +0100

    My Doctor

--- routers/index.js
+++ routers/index.js
@@ -1,30 +1,46 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
+const os = require('os');
 const UserController = require('../controllers/UserController')
 const FilesController = require('../controllers/FilesController')
 const AuthController = require('../controllers/AuthController')
+const DocController = require('../controllers/DocController')
 
+// Creates an instance and config it to save files in '~/Downloads/uploads' directory
+const uploadPath = path.join(os.homedir(), 'Downloads', 'upload');
+const upload = multer({dest: uploadPath })
 const router = express.Router();
 
 router.post('/login', FilesController.login)
 
 router.post('/user/register', UserController.newUser)
 
-router.get('/userDoctors', AuthController.isAuthenticated, FilesController.getMyDoctors)
+router.post('/doc/register', DocController.newDoc)
 
-router.post('/addDoctor', AuthController.isAuthenticated, FilesController.addDoctor);
+router.get('/userDocs', AuthController.isAuthenticated, UserController.getMyDoctors)
 
-router.get('/user', AuthController.isAuthenticated, UserController.currentUser)
+router.post('/addDocs/:name/:docemail/:medicalLicenceNumber', AuthController.isAuthenticated, UserController.addDoctor);
+
+router.get('/user/me', AuthController.isAuthenticated, UserController.currentUser)
+
+router.get('/doc/me', AuthController.isAuthenticated, DocController.currentDoc)
 
 router.get('/files', AuthController.isAuthenticated, FilesController.getAllFiles)
 
-router.get('/files:name', AuthController.isAuthenticated, FilesController.getFile)
-
-router.get('/files:name', AuthController.isAuthenticated, FilesController.getFile)
+router.get('/files/:name', AuthController.isAuthenticated, FilesController.getFile)
 
 router.get('/allusers', AuthController.isAuthenticated, UserController.getAllUsers);
 
-router.get('/userfiles:userEmail', AuthController.isAuthenticated, FilesController.getUserFile);
+router.get('/userfiles/:userEmail', AuthController.isAuthenticated, FilesController.getUserFile);
 
-router.post('/files:data', AuthController.isAuthenticated, FilesController.postFile);
+router.post('/uploadFiles', AuthController.isAuthenticated, upload.single('file'), FilesController.postFile);
+
+router.get('/doctors/departments', AuthController.isAuthenticated, FilesController.doctorsDepts)
+
+router.get('/alldoctors/:department', AuthController.isAuthenticated, DocController.findDocsByDept)
+
+router.get('/alldoctors', AuthController.isAuthenticated, DocController.findAllDocs)
+
 
 module.exports = router;
--- sample.js
+++ sample.js
@@ -1,3 +1,15 @@
-const fs = require('fs')
+// const fs = require('fs')
 
-console.log(Object.keys(fs))
+// // console.log(Object.keys(fs))
+
+// const name = "Welcome to My Project"
+
+// const result = Buffer.from(name).toString('base64');
+
+// console.log(result)
+
+// "V2VsY29tZSB0byBNeSBQcm9qZWN0"
+
+const os = require('os')
+
+console.log(Object.keys(os))
