02236507f6ce60487bb060c0ba8f74345b543c3a
Bassant <yaralikuslar64@gmail.com>
2024-08-16 15:35:38 +0400

    some advanced designs

--- src/App.js
+++ src/App.js
@@ -14,6 +14,8 @@ import DoctorProfile from './DoctorProfile'; // Import DoctorProfile
 import * as authService from './authService';
 import './App.css';
 import DepartmentDoctors from './DepartmentDoctors';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
 
 // Main page component combining all sections except Departments
 const MainPage = () => {
@@ -60,29 +62,30 @@ function App() {
 
     return (
         <Router>
+            <div className="header-strip">
+                <div id="topbar">
+                    <div class="contact-info">
+                        <i class="bi bi-envelope"><a href="mailto:info@example.com">info@example.com</a></i>
+                        <i class="bi bi-phone"><span>+1 5589 55488 55</span></i>
+                    </div>
+                    <div class="social-links">
+                        <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+                        <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                        <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                        <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                    </div>
+                </div>
+            </div>
+
             <Navbar />
             <div className="app-container">
-                <header>
-                    <div id="wlc">
-                        <h1>My Doctor</h1>
-                        <h2>We Help you reach your Doctor!</h2>
-                    </div>
-                    {isAuthenticated && (
-                        <>
-                            <div id="wlcmsg">
-                                <p>Welcome back, {currentUser?.name}</p>
-                                <button onClick={handleLogout}>Logout</button>
-                            </div>
-                        </>
-                    )}
-                </header>
                 <Routes>
                     <Route path="/" element={<MainPage />} />
                     <Route path="/about" element={<About />} />
                     <Route path="/services" element={<Services />} />
                     <Route path="/contact" element={<Contact />} />
                     <Route path="/departments" element={<Departments />} />
-                    <Route path="/doctors/:department" element={<DepartmentDoctors/>} />
+                    <Route path="/doctors/:department" element={<DepartmentDoctors />} />
                     <Route path="/login" element={
                         isAuthenticated ? 
                         (currentUser?.role === 'doctor' ? <DoctorProfile currentUser={currentUser} /> : <PatientProfile currentUser={currentUser} />) 
