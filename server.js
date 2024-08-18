5849a9c54bf78905a73ae60dbede01d997871bb6
Adejare <adejare.arasheed@gmail.com>
2024-08-18 13:06:20 +0100

    Server

--- server.js
+++ server.js
@@ -2,22 +2,26 @@ const express = require('express');
 const session = require('express-session');
 const RedisStore = require('connect-redis').default;
 const  redis = require('redis');
-const router = require('./routers/index');
+const userRouter = require('./routers/userRoutes');
+const doctorRouter = require('./routers/doctorRoutes');
+const fileRouter = require('./routers/fileRoutes');
 
 const PORT = parseInt(process.env.PORT, 10) || 5000;
 
 const app = express();
+// Set-up for Json requests
+app.use(express.json());
+
 
 const redisClient = redis.createClient();
+redisClient.on('connect', () => console.log('Redis Connected to DB'));
+redisClient.on('error', () => console.error('Redis Failed to connect to DB'));
 (async () => {
   // wait for redis to connect
   await redisClient.connect();
 })();
 
-redisClient.on('connect', () => console.log('Redis Connected to DB'));
-redisClient.on('error', () => console.error('Redis Failed to connect to DB'));
-
-app.use(express.json());
+// Set-up Session Handling on Redis
 app.use(session({
   store: new RedisStore({ client: redisClient, ttl: 3600 }),
   secret: process.env.SECRET || 'myVerySecretDoctor"sKey',
@@ -25,7 +29,11 @@ app.use(session({
   saveUninitialized: false, // Don't create session until something is stored
   cookie: { secure: false, maxAge: 3600 * 1000 } // allows http. browser's cookie ttl is 1hr
 }));
-app.use('/', router);
+
+// routers from user, file and doctor
+app.use('/', userRouter);
+app.use('/', doctorRouter);
+app.use('/', fileRouter);
 
 
 app.listen(PORT, () => {
