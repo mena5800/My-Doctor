bd64a4aa384f09d6e3b4d1dbc39b1004866b0c4e
mena5800 <mr.safwat55@gmail.com>
2024-08-23 12:01:37 +0300

    add method to delete user

--- backend/routers/userRoutes.js
+++ backend/routers/userRoutes.js
@@ -10,6 +10,6 @@ Router.get("/current", AuthMiddleware.isAuthenticated, UserController.currentUse
 Router.get("/profile", AuthMiddleware.isAuthenticated, UserController.getProfile);
 Router.put('/profile', AuthMiddleware.isAuthenticated, UserController.updateProfile);
 Router.get("/session", AuthMiddleware.isAuthenticated, UserController.checkSession);
+Router.delete("/delete",AuthMiddleware.isAuthenticated, UserController.deleteUser);
 Router.get("/logout",AuthMiddleware.isAuthenticated, AuthMiddleware.deleteToken);
-
 module.exports = Router;
