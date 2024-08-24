9ebc73f9a45f558ee4f4b3065cbc8dfe2723cd18
Bassant <yaralikuslar64@gmail.com>
2024-08-24 20:30:31 +0400

    contact us section

--- frontend/src/Contact.js
+++ frontend/src/Contact.js
@@ -6,6 +6,32 @@ const Contact = () => {
         <div id="contact">
             <h1>Contact Us</h1>
             <p>Reach out to us with any questions or concerns.</p>
+            <div className="team-container">
+                <div className="team-member">
+                    <img src={require('./img/bassant.png')} alt="Bassant Aboelhassan" />
+                    <h2>Bassant Aboelhassan</h2>
+                    <p>Front-End Developer</p>
+                    <a href="https://github.com/bass3fas" target="_blank" rel="noopener noreferrer" className="github-icon">
+                        <img src={require('./img/github-icon.png')} alt="GitHub" />
+                    </a>
+                </div>
+                <div className="team-member">
+                    <img src={require('./img/mina.png')} alt="Mina Safwat" />
+                    <h2>Mina Safwat</h2>
+                    <p>Back-End Developer</p>
+                    <a href="https://github.com/mena5800" target="_blank" rel="noopener noreferrer" className="github-icon">
+                        <img src={require('./img/github-icon.png')} alt="GitHub" />
+                    </a>
+                </div>
+                <div className="team-member">
+                    <img src={require('./img/favicon.png')} alt="Adejare Abdul Rasheed" />
+                    <h2>Adejare Abdul Rasheed</h2>
+                    <p>Back-End Developer</p>
+                    <a href="https://github.com/Adejare77" target="_blank" rel="noopener noreferrer" className="github-icon">
+                        <img src={require('./img/github-icon.png')} alt="GitHub" />
+                    </a>
+                </div>
+            </div>
         </div>
     );
 };
