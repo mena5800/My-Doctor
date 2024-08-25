7d2d7ec5d2398e04c5635f33d8ed79afdfc974a4
mena5800 <mr.safwat55@gmail.com>
2024-08-25 23:33:43 +0300

    edit db to use mongodb atlas

--- backend/utils/db.js
+++ backend/utils/db.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     // Replace with your actual MongoDB connection string
-    const conn = await mongoose.connect('mongodb://localhost:27017/myDoctor', {
+    const conn = await mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost:27017/myDoctor', {
       // Removed deprecated options
       
     });
